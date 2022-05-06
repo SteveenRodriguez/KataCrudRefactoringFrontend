159f943aa304be64f0df9043b67e6efa3533efa8
steveen <ss.rodriguez00021@gmail.com>
2022-05-06 00:18:15 -0500

    edit todos

---
+++ src/components/EditToDo.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const EditToDo = () => {
+    const cancelar = (e) => {
+        e.preventDefault();
+    };
+
+    const editTodo = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div>
+            <form>
+                <input type="text" placeholder="Nombre a cambiar"></input>
+                <button onClick={editTodo}>Aceptar</button>
+                <button onClick={cancelar}> Cancelar</button>
+            </form>
+        </div>
+    );
+};
+
+export default EditToDo;
