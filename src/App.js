635526c453f5fddefad887a899b98120e1fc28dd
steveen <ss.rodriguez00021@gmail.com>
2022-05-05 23:03:11 -0500

    creación componente CreateList, crea listas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import CreateList from './components/CreateList';
 
 const App = () => (
     <>
         <h1>To-Do apllication</h1>
+        <CreateList/>
     </>
 );
 
---
+++ src/components/CreateList.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+//creación función CreateList
+const CreateList = () => {
+
+    //se crea la función lista, con su estado inicial en cero
+    const [lista, setLista] = useState("");
+
+    const submit = (e) => {
+        const headers = { "Content-Type": "application/json" };
+        //objeto respuesta, lo pasamos al fetch
+        let requestParameters = {
+            method: "POST",
+            headers,
+            body: JSON.stringify({ //convertimos el body en un Json
+                name: lista,
+            }),
+        };
+        //llamamos a nuesta api, variable de entorno y le pasamos los parámetros
+        fetch(`${process.env.REACT_APP_HOST_API}/api/list`, requestParameters)
+            .then(() => { console.log("Lista Creada"); });
+    };
+
+    return (
+        <form onSubmit={submit}>
+            <h3>Crear nueva lista de tareas:</h3>
+            <input
+                type="text"
+                required
+                value={lista}
+                placeholder="Nombre Lista"
+                onChange={(e) => setLista(e.target.value)}
+            />
+            <input type="submit" className="btn btn-primary" value="Guardar" />
+        </form>
+    );
+
+};
+
+export default CreateList;
