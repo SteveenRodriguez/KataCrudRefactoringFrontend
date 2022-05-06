d9e914acbcca10c54a476b85a458d25d649992e6
steveen <ss.rodriguez00021@gmail.com>
2022-05-05 23:46:58 -0500

    dibuja en el dom las listas con sus todos

---
+++ src/components/List.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import CreateToDo from './CreateToDo';
+import ToDo from './Todo';
+
+const List = ({ list, todos }) => {
+    return (
+        <div className="card card-body border-primary mb-3 mt-2">
+            <div>
+                <h1>{list.name}</h1>
+                <button className="btn btn-danger">Eliminar Lista</button>
+                <CreateToDo
+                    {...list}
+                />
+            </div>
+
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>ID</td>
+                            <td>Tarea</td>
+                            <td>¿Completado?</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            todos.map(todo => (
+                                <ToDo
+                                    key={todo.idToDo}
+                                    {...todo}
+                                />
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    );
+};
+
+export default List;
